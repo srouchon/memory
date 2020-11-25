cf9de8b599f33a0d37b64e978ddcf09b871d00bc
sophie rouchon <sophie.rouchon@gmail.com>
2020-11-25 09:23:07 +0100

    import modif from original repo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import GuessCount from './GuessCount';
-import React, { Component } from 'react';
-import './App.css';
-import Card from './Card';
+import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
+import './App.css'
+
+import Card from './Card'
+import GuessCount from './GuessCount'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
 
 class App extends Component {
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
   handleCardClick(card) {
     console.log(card, 'clicked')
   }
-  
+
   render() {
     const won = new Date().getSeconds() % 2 === 0
     return (
-    <div className="memory">
-      <GuessCount guesses={0}/>
-      <Card card='🎉' feedback='hidden' onClick={this.handleCardClick} />
-      <Card card='😊' feedback='justMatched' onClick={this.handleCardClick} />
-      <Card card='👽' feedback='justMismatched' onClick={this.handleCardClick} />
-      <Card card='👄' feedback='visible' onClick={this.handleCardClick} />
-      <Card card='🦁' feedback='hidden' onClick={this.handleCardClick} />
-      <Card card='🌲' feedback='justMatched' onClick={this.handleCardClick} />
-      {won && <p>GAGNÉ !</p>}
-    </div>
+      <div className="memory">
+        <GuessCount guesses={0} />
+        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card
+          card="💖"
+          feedback="justMismatched"
+          onClick={this.handleCardClick}
+        />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>GAGNÉ !</p>}
+      </div>
     )
   }
-};
+}
 
-export default App;
+export default App
