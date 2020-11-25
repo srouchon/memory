caff9e2970c9e94b640c0c5d314b6f40f9995645
sophie rouchon <sophie.rouchon@gmail.com>
2020-11-25 08:19:46 +0100

    add cards and feedbacks settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import GuessCount from './GuessCount';
+import React, { Component } from 'react';
 import './App.css';
+import Card from './Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+    <div className="memory">
+      <GuessCount guesses={0}/>
+      <Card card='🎉' feedback='hidden' />
+      <Card card='😊' feedback='justMatched' />
+      <Card card='👽' feedback='justMismatched' />
+      <Card card='👄' feedback='visible' />
+      <Card card='🦁' feedback='hidden' />
+      <Card card='🌲' feedback='justMatched' />
     </div>
-  );
-}
+    )
+  }
+};
 
 export default App;
